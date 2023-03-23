b2911b1d82741f8470d78db968fe6a063b20c391
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-23 15:12:03 -0700

    Check if c is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+* main - Checks if c is digit
+*
+* @c: integer to be checked
+*
+* Return: 1 if C, else 0
+*/
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+else 
+return (0);
+}
