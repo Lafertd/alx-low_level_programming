a6de9f06babfd2ee1784219356fd3d39a87fb4c5
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-23 15:56:33 -0700

    print numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,12 @@ include "main.h"
 
 /**
 * print_numbers - multiply 2 integers
-* Description: print numbers from 0-9 
+* Description: print numbers from 0-9
 */
 
 void print_numbers(void);
 {
-for(i = '0'; i <= '9' ; i++)
+for (i = '0'; i <= '9' ; i++)
 {
 _putchar(i);
 }
