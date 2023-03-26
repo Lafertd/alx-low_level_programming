ebaedd00d79d997f73311d9d86421a5679c62c89
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-25 19:11:44 -0700

    hi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
