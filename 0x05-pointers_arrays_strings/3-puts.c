2f8ed0fd13ff7e3241839425f1510216e976dcf9
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-27 18:54:01 -0700

    updated

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *
+ * Return: char 
+ */
+void _puts(char *str)
+{
+_puts("I do not fear computers. I fear the lack of them - Isaac Asimov");
+}
