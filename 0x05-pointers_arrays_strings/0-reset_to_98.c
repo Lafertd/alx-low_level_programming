78ccd6b6ae55e78150ce4006c2ff5666d9851398
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-27 17:19:30 -0700

    updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,8 +9,8 @@ int main(void)
     int n;
 
     n = 402;
-    printf("n=%d\n", n);
+    _putchar("n=%d\n", n);
     reset_to_98(&n);
-    printf("n=%d\n", n);
+    _putchar("n=%d\n", n);
     return (0);
 }
