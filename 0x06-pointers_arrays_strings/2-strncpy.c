77b88405f5b4cab2c731fb301f0f9f989b011803
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-31 19:35:16 -0700

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,5 +20,5 @@ char *_strncpy(char *dest, char *src, int n)
 	       {       i++;
                    dest[i] = '\0';
 	       }
-               return dest;
+               return (dest);
            }
