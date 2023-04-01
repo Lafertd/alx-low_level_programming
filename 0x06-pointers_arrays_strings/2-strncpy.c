08784864ab9c086d499aed70103a04a487c9bd76
lafertd <achrafprofessionalinfo@gmail.com>
2023-03-31 19:34:12 -0700

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
                int i = 0;
 
                while (i < n && src[i] != '\0')
-		       i++;
-                   dest[i] = src[i];
+	       {       i++;
+	          dest[i] = src[i];
+	       }
                while (i < n)
-		       i++;
+	       {       i++;
                    dest[i] = '\0';
-
+	       }
                return dest;
            }
