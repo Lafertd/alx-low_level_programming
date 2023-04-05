b04bbbc214e829eeb77e3672c7f5a8bf2ce7f202
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-05 10:40:26 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,8 +7,9 @@
  */
 void _puts_recursion(char *s) {
     if (*s != '\0') {
-        putchar(*s);
+	   return 0;
+    }
+    {   putchar(*s);
         _puts_recursion(s + 1);
-    } else {
-        putchar('\n');
+    }
 }
