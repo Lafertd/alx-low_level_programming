86520c0941793d08c25d4663fbdd0b2e3a663686
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-05 10:38:47 -0700

    updated

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include main.h
+/**
+ * _puts_recursion - function that prints a string
+ * @s: pointer to char variable
+ *
+ * Return: return 0 (Solved)
+ */
+void _puts_recursion(char *s) {
+    if (*s != '\0') {
+        putchar(*s);
+        _puts_recursion(s + 1);
+    } else {
+        putchar('\n');
+}
