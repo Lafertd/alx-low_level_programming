fbe0117642d5dfa6182f21f7899a055941d8ed44
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-09 10:57:09 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,6 +7,12 @@
  * Return: return 0(Solved)
  */
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
+{
+	int i;
+	for(i = 0; i < argc; i++)
+	{
 	printf("%s\n", argv[0]);
+	}
 	return 0;
+}
