99c583c950aeddc39d645cd9613c06b2cd053109
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-09 11:25:56 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,19 @@
 
 /**
  * main - function that print the name of the program
- * 
+ * @argc: the number of arguments passed
+ * @argv: the argument names passed
+ *
  * Return: return 0(Solved)
  */
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int i;
-	for(i = 0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
 	printf("%s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
