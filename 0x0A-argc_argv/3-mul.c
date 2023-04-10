329d087ef7b75c8675fc138a562b3c74ac547f6f
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-10 07:59:32 -0700

    updated

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - function that print the name of the program
+ * @argc: the number of arguments passed
+ * @argv: the argument names passed
+ *
+ * Return: return 0(Solved), 1(Error)
+ */
+int main(int argc, char **argv)
+{
+int a = 0, b = 0;
+int result = a * b;
+if (argc == 3)
+for(i = 0; i < strlen(argv); i++ )
+{
+int a = a * 10 + (argv[i] - '0');
+}
+for(j = 0; j < strlen(argv); j++);
+{
+int b = b * 10 + (agrv[j] - '0');
+}
+{
+printf("%d\n", result);
+}
+return (0);
+if (argc != 3)
+{
+printf("Error");
+}
+return (1);
+}
