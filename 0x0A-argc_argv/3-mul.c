a6c65b5823a30752216055412056e314a35d02e7
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-10 09:05:20 -0700

    updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,8 +16,8 @@ printf("Error\n");
 return (1);
 }
 int a, b;
-int a = atoi(argv[1]);
-int b = atoi(argv[2]);
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 int result = a * b;
 {
 printf("%d\n", result);
