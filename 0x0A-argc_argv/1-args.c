48315d7c43d1017e29184d4407d818671cf38694
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-09 18:53:46 -0700

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - function that print the name of the program
+ * @argc: the number of arguments passed
+ * @argv: the argument names passed
+ *
+ * Return: return 0(Solved)
+ */
+
+int main(int argc, char **argv)
+{
+        int i;
+
+       printf("%d\n", i);
+       return (0);
+}
