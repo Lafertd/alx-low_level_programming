21fe4c199c9d5ed4ea2897abe6345f6c1cbd47b3
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-17 16:39:45 -0700

    updated

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that print the file name,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
