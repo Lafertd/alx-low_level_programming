85e10982010632fba1d2c845cbf548b23979e457
lafertd <achrafprofessionalinfo@gmail.com>
2023-04-20 19:10:44 -0700

    updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
 /**
  * sum_them_all - function that adds the arguments passed to it
  * @n: the arguments counter
+ *
  * Return: if n == 0 return (0) otherwise return sum;
  */
 
@@ -15,10 +16,10 @@ int sum_them_all(const unsigned int n, ...)
 	}
 
 	va_list ap;
-	va_start (ap , n);
+	va_start (ap, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap , const unsigned int);
+		sum += va_arg (ap, const unsigned int);
 	va_end (ap);
 	return sum;
 }
