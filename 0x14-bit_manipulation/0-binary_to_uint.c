797afde879ae53e4558f1625b0da2b49b944fea6
lafertd <achrafprofessionalinfo@gmail.com>
2023-05-05 05:03:53 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,9 +5,11 @@
  * binary_to_uint - function that takes a binary number
  * and convert it to decimal number
  * @b: string pointer that points at the binary numbers
+ * Description: this function calculates the unit of binary (base^n * *b)
  * Return: return(Converted number: if the binary are 0,1)
  * otherwise it return 0
  **/
+
 unsigned int binary_to_uint(const char *b)
 {
 const char *str;
@@ -16,17 +18,20 @@ int i, j;
 int n = i - 1;
 int uint;
 
-if(*str != '0' || *str != '1' || str != NULL){
-return(0);
+if (*str != '0' || *str != '1' || str != NULL)
+{
+return (0);
 }
 else
-for(i = strlen(const char *str); str != NULL; i--){
-for(j = 0; j <= n; j++){
+for (i = strlen(const char *str); str != NULL; i--)
+{
+for (j = 0; j <= n; j++)
+{
 uint = (n * n);
 uint += uint;
 }
-uint = (uint * *str);
+uint = ((uint)*(*str));
 uint += uint;
 }
-return(uint);
+return (uint);
 }
